0927b2c108bb2a933569f4b6d0ff64a667af2ee1
HannahDeWachter <r0738008@student.thomasmore.be>
2020-03-22 11:49:51 +0100

    connecting to weather api

---
+++ lab 4/app.js
@@ -0,0 +1,36 @@
+class Weather {
+    constructor() {
+        this.getLocation();
+        this.lat;
+        this.lng;
+    }
+
+    getLocation() {
+        navigator.geolocation.getCurrentPosition(this.gotLocation.bind(this), this.errorLocation.bind(this));
+    }
+
+    gotLocation(result) {
+        this.lat = result.coords.latitude;
+        this.lng = result.coords.longitude;
+        this.getWeather();
+    }
+
+    getWeather() {
+        let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/fdc76459345f51e97a02c4495a00f0e0/${this.lat},${this.lng}?units=si`;
+        fetch(url).then(response => {
+            return response.json();
+        }).then(data => {
+            let summaryText = data.currently.summary;
+            let tempDegrees = Math.round(data.currently.temperature);
+            document.querySelector("#weather").innerHTML = summaryText;
+        }).catch(err => {
+            console.log(err);
+        });
+    }
+
+    errorLocation(err) {
+        console.log(err);
+    }
+}
+
+let weather = new Weather();
