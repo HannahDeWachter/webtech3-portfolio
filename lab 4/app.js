9e7d5efec3d359bea7134d62613313eb81d2ff18
HannahDeWachter <r0738008@student.thomasmore.be>
2020-03-22 13:10:50 +0100

    weather icons added

--- lab 4/app.js
+++ lab 4/app.js
@@ -25,7 +25,8 @@ class Weather {
 
             let summaryText = data.currently.summary;
             let tempDegrees = Math.round(data.currently.temperature);
-            document.querySelector("#weather").innerHTML = tempDegrees + " °C" + "<br />" + summaryText;
+            document.querySelector("#temp").innerHTML = tempDegrees + " °C";
+            document.querySelector("#summary").innerHTML = summaryText;
 
             // jas: https://s7d1.scene7.com/is/image/TheNorthFaceBrand/3ERL1SK_prod1?$transparent-png$&wid=600
             // bril: https://www.merkbrillen.nl/uploads/pagetree/images/Ray-Ban-aviator-3025.png?w=1024&algo=fill
@@ -33,15 +34,35 @@ class Weather {
 
             switch (weatherIcon) {
                 case "clear-day":
+                    document.getElementById("icon").src = "https://image.flaticon.com/icons/svg/106/106061.svg";
                     document.getElementById("article").src = "https://www.merkbrillen.nl/uploads/pagetree/images/Ray-Ban-aviator-3025.png?w=1024&algo=fill";
                     break;
                 case "partly-cloudy-day":
+                    document.getElementById("icon").src = "https://image.flaticon.com/icons/svg/106/106068.svg";
                     document.getElementById("article").src = "https://cdn.zeeman.com/media/catalog/product/cache/5050dbc22447fab33b3d2c8a729076f7/2/0/2020020684651_Front_01.png";
+                    break;
                 case "rain":
+                    document.getElementById("icon").src = "https://image.flaticon.com/icons/svg/106/106059.svg";
+                    document.getElementById("article").src = "https://s7d1.scene7.com/is/image/TheNorthFaceBrand/3ERL1SK_prod1?$transparent-png$&wid=600";
+                    break;
                 case "wind":
+                    document.getElementById("icon").src = "https://image.flaticon.com/icons/svg/106/106040.svg";
+                    document.getElementById("article").src = "https://s7d1.scene7.com/is/image/TheNorthFaceBrand/3ERL1SK_prod1?$transparent-png$&wid=600";
+                    break;
                 case "cloudy":
+                    document.getElementById("icon").src = "https://image.flaticon.com/icons/svg/106/106064.svg";
+                    document.getElementById("article").src = "https://s7d1.scene7.com/is/image/TheNorthFaceBrand/3ERL1SK_prod1?$transparent-png$&wid=600";
+                    break;
                 case "fog":
+                    document.getElementById("icon").src = "https://image.flaticon.com/icons/svg/106/106052.svg";
+                    document.getElementById("article").src = "https://s7d1.scene7.com/is/image/TheNorthFaceBrand/3ERL1SK_prod1?$transparent-png$&wid=600";
+                    break;
+                case "snow":
+                    document.getElementById("icon").src = "https://image.flaticon.com/icons/svg/106/106056.svg";
+                    document.getElementById("article").src = "https://s7d1.scene7.com/is/image/TheNorthFaceBrand/3ERL1SK_prod1?$transparent-png$&wid=600";
+                    break;
                 default:
+                    document.getElementById("icon").src = "https://image.flaticon.com/icons/svg/106/106068.svg";
                     document.getElementById("article").src = "https://s7d1.scene7.com/is/image/TheNorthFaceBrand/3ERL1SK_prod1?$transparent-png$&wid=600";
                     break;
             }
