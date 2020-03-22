eabc563fe6cc565239aefd79330a6c9f7c1c9820
HannahDeWachter <r0738008@student.thomasmore.be>
2020-03-22 12:48:22 +0100

    switch case: show img for certain weather

--- lab 4/app.js
+++ lab 4/app.js
@@ -20,9 +20,32 @@ class Weather {
         fetch(url).then(response => {
             return response.json();
         }).then(data => {
+            console.log(data);
+            let weatherIcon = data.currently.icon;
+
             let summaryText = data.currently.summary;
             let tempDegrees = Math.round(data.currently.temperature);
             document.querySelector("#weather").innerHTML = tempDegrees + " °C" + "<br />" + summaryText;
+
+            // jas: https://s7d1.scene7.com/is/image/TheNorthFaceBrand/3ERL1SK_prod1?$transparent-png$&wid=600
+            // bril: https://www.merkbrillen.nl/uploads/pagetree/images/Ray-Ban-aviator-3025.png?w=1024&algo=fill
+            // tshirt: https://cdn.zeeman.com/media/catalog/product/cache/5050dbc22447fab33b3d2c8a729076f7/2/0/2020020684651_Front_01.png
+
+            switch (weatherIcon) {
+                case "clear-day":
+                    document.getElementById("article").src = "https://www.merkbrillen.nl/uploads/pagetree/images/Ray-Ban-aviator-3025.png?w=1024&algo=fill";
+                    break;
+                case "partly-cloudy-day":
+                    document.getElementById("article").src = "https://cdn.zeeman.com/media/catalog/product/cache/5050dbc22447fab33b3d2c8a729076f7/2/0/2020020684651_Front_01.png";
+                case "rain":
+                case "wind":
+                case "cloudy":
+                case "fog":
+                default:
+                    document.getElementById("article").src = "https://s7d1.scene7.com/is/image/TheNorthFaceBrand/3ERL1SK_prod1?$transparent-png$&wid=600";
+                    break;
+            }
+
         }).catch(err => {
             console.log(err);
         });
