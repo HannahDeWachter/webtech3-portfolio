6bc01dcb2cad311a4dc3873c78354111586088fd
HannahDeWachter <r0738008@student.thomasmore.be>
2020-04-07 10:09:01 +0200

    messages route in app.js not index.js

--- lab 5/routes/index.js
+++ lab 5/routes/index.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const apiV1Messagesrouter = require('./routers/api/v1/messages')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-app.use('/api/v1/messages', apiV1Messagesrouter)
 
 module.exports = router;
