6fdc8def6aaa8800efc2a19fe088a6e9e930259d
HannahDeWachter <r0738008@student.thomasmore.be>
2020-04-07 10:47:20 +0200

    install nodemon

--- lab 5/controllers/api/v1/messages.js
+++ lab 5/controllers/api/v1/messages.js
@@ -1,20 +1,38 @@
 const getAllMessages = (req, res) => {
-    res.json({ "message": "GETTING messages" });
+    res.json({
+        "status": "succes",
+        "message": "GETTING messages"
+    });
 }
 const getIdMessage = (req, res) => {
-    res.json({ "message": "GETTING message with ID " } + req.params.id);
+    res.json({
+        "status": "succes",
+        "message": "GETTING message with ID " + req.params.id
+    });
 }
 const create = (req, res) => {
-    res.json({ "message": "POSTING a new message for user Pikachu" });
+    res.json({
+        "status": "succes",
+        "message": "POSTING a new message for user Pikachu"
+    });
 }
 const update = (req, res) => {
-    res.json({ "message": "UPDATING a message with ID " } + req.params.id);
+    res.json({
+        "status": "succes",
+        "message": "UPDATING a message with ID " + req.params.id
+    });
 }
 const remove = (req, res) => {
-    res.json({ "message": "DELETING a message with ID " } + req.params.id);
+    res.json({
+        "status": "succes",
+        "message": "DELETING a message with ID " + req.params.id
+    });
 }
 const getUserMessages = (req, res) => {
-    res.json({ "message": "GETTING message for username " } + req.params.username);
+    res.json({
+        "status": "succes",
+        "message": "GETTING message for username " + req.params.username
+    });
 }
 
 module.exports.getAllMessages = getAllMessages;
