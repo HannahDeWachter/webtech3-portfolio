f2d4e01cfc1992078fa367403d6b82ac2a1c1c13
HannahDeWachter <r0738008@student.thomasmore.be>
2020-04-10 12:17:52 +0200

    json responses in controller

---
+++ lab 5/controller/api/v1/messages.js
@@ -0,0 +1,20 @@
+const getAllMessages = (req, res) => {
+    res.json('{“message”: “GETTING messages” }');
+}
+const getIdMessage = (req, res) => {
+    res.json('{ “message”: “GETTING message with ID ” }' + req.params.id);
+}
+const create = (req, res) => {
+    res.json('{“message”: “POSTING a new message for user Pikachu”}');
+}
+const update = (req, res) => {
+    res.json('{“message”: “UPDATING a message with ID ”}' + req.params.id);
+}
+const remove = (req, res) => {
+    res.json('{“message”: “DELETING a message with ID ”}' + req.params.id);
+}
+const getUserMessages = (req, res) => {
+    res.json('{“message”: “GETTING message for username ”}' + req.params.username);
+}
+
+module.exports.getAllMessages = getAllMessages;
